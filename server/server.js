072f311e44077c6b5836f4806bf54c3883cd9ed7
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-09-19 22:33:35 -0700

    fixed server error

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT;
+// const port = process.env.PORT || 3000;
 
 app.use(express.static('dist'));
 
-app.listen(port, () => console.log(`*** App listening on port ${port}! ***`));
+app.listen(process.env.PORT);
+// , () => console.log(`*** App listening on port ${port}! ***`));
